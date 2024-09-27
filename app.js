a3bc673e25353d772c37c3f863e056a89bb04bc5
Siddharth Sharma <siddharth54330@gmail.com>
2024-09-27 21:43:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World! - V2(This is a New Commit Which Will Trigger the Pipeline)'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
